fd54f5e551d73be7071cf58f821b1fa28a0fcc10
chenjuntao <1769974308@qq.com>
2021-12-01 17:44:46 +0800

    maven 打包docker镜像

---
+++ docker-registry/src/main/java/com/taotao/java/DockerRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.taotao.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerRegistryApplication.class, args);
+    }
+
+}
---
+++ docker-registry/src/main/java/com/taotao/java/DockerRegistryController.java
@@ -0,0 +1,22 @@
+package com.taotao.java;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @auther ASUS
+ * @date 2021/11/30
+ */
+@Controller
+@RequestMapping("/docker")
+public class DockerRegistryController {
+
+    @GetMapping("/registry")
+    @ResponseBody
+    public String registry(){
+
+        return  "maven plugin packaging  image successful ！！！ ";
+    }
+}
