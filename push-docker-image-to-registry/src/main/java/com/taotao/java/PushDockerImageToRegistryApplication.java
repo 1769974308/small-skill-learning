a03a1871d7d0a8b85a4dc270212734712d15e048
chenjuntao <1769974308@qq.com>
2021-12-02 11:33:54 +0800

    更新项目名maven-package-docker-image

---
+++ push-docker-image-to-registry/src/main/java/com/taotao/java/PushDockerImageToRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.taotao.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushDockerImageToRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushDockerImageToRegistryApplication.class, args);
+    }
+
+}
